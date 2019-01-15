54b0c6868f0725600a75b6f080f9f4674e4bc40b
Thu Ta Naing <thutanaingytu.997@gmail.com>
2019-01-15 22:24:04 +0630

    commit

--- htdocs/wp-config.php
+++ htdocs/wp-config.php
@@ -91,7 +91,7 @@ define( 'PATH_CURRENT_SITE', '/' );
 define( 'SITE_ID_CURRENT_SITE', 1 );
 define( 'BLOG_ID_CURRENT_SITE', 1 );
 
-
+define( 'NOBLOGREDIRECT', 'http://www.18.191.245.118.xip.io' );
 define('FS_METHOD', 'direct');
 
 
