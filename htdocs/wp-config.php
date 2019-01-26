c8df5b19164e5e79470b1b86ab4477198a362ccf
Thu Ta Naing <thutanaingytu.997@gmail.com>
2019-01-26 23:49:53 +0630

    commit

--- htdocs/wp-config.php
+++ htdocs/wp-config.php
@@ -91,7 +91,7 @@ define( 'PATH_CURRENT_SITE', '/' );
 define( 'SITE_ID_CURRENT_SITE', 1 );
 define( 'BLOG_ID_CURRENT_SITE', 1 );
 
-//This code for redirect from wp-signup page
+//This code for redirect from wp-signup page.
 define( 'NOBLOGREDIRECT', 'http://www.sensesignagesolutions.com' );
 define('FS_METHOD', 'direct');
 
