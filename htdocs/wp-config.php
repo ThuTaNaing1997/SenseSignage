a784a80b9b860c39a57e23333d36f2c761a1b9ec
Thu Ta Naing <thutanaingytu.997@gmail.com>
2019-01-28 15:43:10 +0630

    commit

--- htdocs/wp-config.php
+++ htdocs/wp-config.php
@@ -91,7 +91,7 @@ define( 'PATH_CURRENT_SITE', '/' );
 define( 'SITE_ID_CURRENT_SITE', 1 );
 define( 'BLOG_ID_CURRENT_SITE', 1 );
 //new code
-//define( ‘COOKIE_DOMAIN’, $_SERVER[ ‘HTTP_HOST’ ] );
+define( ‘COOKIE_DOMAIN’, $_SERVER[ ‘HTTP_HOST’ ] );
 define('COOKIE_DOMAIN', false);
 
 
